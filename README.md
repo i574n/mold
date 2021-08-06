d66c92e8827685f1dfd470b1e6d5277a4a54f487
fc1943s <root@fc1943s.dev>
2021-08-06 16:28:17 -0300

    windows

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 ```
 cd $env:tmp
-curl -LO fc1943s.github.io/mold/mold-install-windows.ps1
-sh mold-install-windows.ps1
+curl.exe -LO fc1943s.github.io/mold/mold-install-windows.ps1
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+./mold-install-windows.ps1
 ```
 
 
--- home/mold-install-windows.ps1
+++ home/mold-install-windows.ps1
@@ -1,15 +1,10 @@
 Write-Output 'Installing mold...'
 
-if ($true -or !$( which scoop ))
-{
-	if (!$( which scoop ))
-	{
-		Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
-	}
-	
-	Read-Host -Prompt "Script finished. Please restart manually. Press any key to close"
-}
-else
+Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
+
+if ($( gcm scoop ))
 {
 	scoop update
 }
+
+Read-Host -Prompt "Script finished. Please restart manually. Press any key to close"
