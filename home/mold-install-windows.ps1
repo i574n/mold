13fcecef9e9ff19aac406ce4487a9f89ec156318
fc1943s <root@fc1943s.dev>
2021-08-06 17:37:50 -0300

    windows

--- home/mold-install-windows.ps1
+++ home/mold-install-windows.ps1
@@ -1,9 +1,13 @@
 Write-Output 'Installing mold...'
 
-Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
-scoop install main/7zip
-scoop install main/git
-scoop bucket add mold https://github.com/fc1943s/mold.git
+if(!$(gcm scoop)) {
+	Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
+
+	scoop install main/7zip
+	scoop install main/git
+
+	scoop bucket add mold https://github.com/fc1943s/mold.git
+}
 
 $windowsRoot = "$env:userprofile/scoop/buckets/mold/src/windows"
 
@@ -13,5 +17,6 @@ dotnet fsi $windowsRoot/install.fsx
 
 . $windowsRoot/install-scoop-extras.ps1
 
+scoop bucket add extras
 
 Read-Host -Prompt "Script finished. Restart manually if needed. Press any key to close"
