c2b18dfeec805b2a3a3d8567e84143c9d8b252dc
fc1943s <fc1943s@users.noreply.github.com>
2023-03-08 15:32:26 +0000

    deploy: 7fa12cf559c49bfdbf0ed4298ec95f4129fa773a

--- linux/path/gitclone.sh
+++
@@ -1,6 +0,0 @@
-# @echo off
-# echo %1
-# set dist="~/home/fs/repos"
-# echo %dist%
-# cd /D %dist%
-# git clone %1
---
+++ windows/install.ps1
@@ -0,0 +1,34 @@
+echo 'mold ()'
+
+$env:scoop = "$env:userprofile/scoop"
+$env:mold = "$env:scoop/buckets/mold"
+
+if(!$(gcm scoop)) {
+    irm get.scoop.sh | iex
+
+    [Environment]::SetEnvironmentVariable('SCOOP', $env:scoop, 'User')
+    [Environment]::SetEnvironmentVariable('MOLD', $env:mold, 'User')
+
+    [Environment]::SetEnvironmentVariable('PATH', "$env:PATH;" +
+        "$env:mold/home/windows/path;" +
+        "$env:scoop/persist/rustup/.cargo/bin;" +
+        "$env:scoop/apps/nvm/current/nodejs/nodejs;" +
+        "$env:scoop/apps/cygwin/current/root/bin"
+    , 'User')
+
+}
+
+    scoop install main/7zip
+    scoop install main/git
+
+    scoop config SCOOP_REPO 'https://github.com/i574n/Scoop-Core'
+
+    Get-ChildItem "$env:scoop/shims" -Filter 'scoop.*' | Copy-Item -Destination { Join-Path $_.Directory.FullName (($_.BaseName -replace 'scoop', 'shovel') + $_.Extension) }
+
+    shovel bucket add mold https://github.com/i574n/mold.git
+
+    shovel update
+    shovel status
+    shovel checkup
+
+. "$env:mold/src/windows/install.ps1"
